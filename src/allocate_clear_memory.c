5ddd32abce124573da4a36307d6c49b30ad957e9
Alexandru Miclea <alexandrumiclea@outlook.com>
2024-01-15 12:44:01 +0200

    looks good up to now

--- src/allocate_clear_memory.c
+++ src/allocate_clear_memory.c
@@ -6,10 +6,10 @@ void *allocate_clear_memory(size_t cnt, size_t req_size){
     // hideous way to check for a possible overflow 
     if (cnt * req_size < 0) return NULL;
     
-    block_metadata* block = find_free_block((block_metadata**)heap_begin, req_size * cnt);
+    block_metadata* block = find_free_block((block_metadata*)heap_begin, req_size * cnt);
     // TODO SEE IF IT IS OK
     //printf("adresa de la care fac memset: %p\n", (void*)block + METADATA_SIZE);
-    memset((void*)block + METADATA_SIZE, 0, req_size * cnt);
+    memset((void*)block + METADATA_SIZE, 0, cnt);
 
     return (void*)block + METADATA_SIZE;
 }
--- src/main.c
+++ src/main.c
@@ -9,90 +9,6 @@
 
 int main() {
 
-    // int* first_alloc = allocate_memory(100);
-    // printf("adresa heap: 0x%X\n", heap_begin);
-    // printf("sfarsit heap: 0x%X\n", heap_end);
-    // printf("adresa alloc: 0x%X\n", first_alloc);
-    // printf("dimensiune metadata: 0x%X\n", METADATA_SIZE);
-    // printf("adresa metadata alloc: 0x%X\n", get_address_block(first_alloc));
-
-    // printf("Inceput heap: 0x%X\n", heap_begin);
-    // printf("Sfarsit heap: 0x%X\n", heap_end);
-    // int* smek = allocate_memory(getpagesize() - 2*METADATA_SIZE -1);
-
-    // block_metadata* block_head = smek - 8;
-
-    // block_metadata* pointer = block_head;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // printf("Inceput heap: 0x%X\n", heap_begin);
-    // printf("Sfarsit heap: 0x%X\n", heap_end);
-
-    // int* smek2 = allocate_memory(1);
-
-    // pointer = block_head;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // printf("adresa alloc: 0x%X\n", smek2);
-    // printf("Inceput heap: 0x%X\n", heap_begin);
-    // printf("Sfarsit heap: 0x%X\n", heap_end);
-    // int* smek2 = allocate_memory(sizeof(int) * 3);
-    // printf("adresa alloc: 0x%X\n", smek2);
-    // printf("Inceput heap: 0x%X\n", heap_begin);
-    // printf("Sfarsit heap: 0x%X\n", heap_end);
-
-    // pointer = block_head;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // int* first_alloc = allocate_memory(10 * sizeof(int));
-    // for (int i = 0; i < 10; i++) {
-    //     first_alloc[i] = i + 10;
-    // }
-    // deallocate_memory(first_alloc);
-    // for (int i = 0; i < 10; i++) {
-    //     first_alloc[i] = i + 10;
-    //     printf("%d", first_alloc[i]);
-    // }
-    // printf("\n");
-    // int* second_alloc = allocate_clear_memory(0,10 * sizeof(int));
-    // printf("%p %p", first_alloc, second_alloc);
-    // printf("\n");
-    // for (int i = 0; i < 10; i++) {
-    //     printf("%d", first_alloc[i]);
-    // }
-    // block_metadata* pointer = heap_begin;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // deallocate_memory(first_alloc);
-
-    // pointer = heap_begin;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // deallocate_memory(third_alloc);
-
-    // pointer = heap_begin;
-    // while (pointer != NULL){
-    //     printf("%d %d\n", pointer->size, pointer->free);
-    //     pointer = pointer->next;
-    // }
-
-    // buggy behavior
-
     int* space = allocate_memory(100);
 
     block_metadata* pointer = heap_begin;
@@ -100,6 +16,16 @@ int main() {
         printf("%d %d\n", pointer->size, pointer->free);
         pointer = pointer->next;
     }
+    printf("\n");
+
+    space = allocate_memory(100);
+
+    pointer = heap_begin;
+    while (pointer != NULL){
+        printf("%d %d\n", pointer->size, pointer->free);
+        pointer = pointer->next;
+    }
+    printf("\n");
 
     space = reallocate_memory(space, 20);
 
@@ -108,6 +34,7 @@ int main() {
         printf("%d %d\n", pointer->size, pointer->free);
         pointer = pointer->next;
     }
+    printf("\n");
 
     space = reallocate_memory(space, 120);
 
@@ -116,6 +43,25 @@ int main() {
         printf("%d %d\n", pointer->size, pointer->free);
         pointer = pointer->next;
     }
+    printf("\n");
+
+    deallocate_memory(space);
+
+    pointer = heap_begin;
+    while (pointer != NULL){
+        printf("%d %d\n", pointer->size, pointer->free);
+        pointer = pointer->next;
+    }
+    printf("\n");
+
+    space = allocate_clear_memory(3, sizeof(int));
+
+    pointer = heap_begin;
+    while (pointer != NULL){
+        printf("%d %d\n", pointer->size, pointer->free);
+        pointer = pointer->next;
+    }
+    printf("\n");
 
     return 0;
 }
